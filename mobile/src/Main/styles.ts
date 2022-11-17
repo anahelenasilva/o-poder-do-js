d563e37fb15a43f45593d0bb16601cfa1266eccf
Ana Helena <anahelenarp@hotmail.com>
2022-11-16 22:48:40 -0300

    feat: add mobile

---
+++ mobile/src/Main/styles.ts
@@ -0,0 +1,31 @@
+import styled from "styled-components/native";
+
+import { Platform, StatusBar } from "react-native";
+
+const isAndroid = Platform.OS === "android";
+
+const currentHeight = StatusBar.currentHeight;
+
+export const Container = styled.SafeAreaView`
+    margin-top: ${isAndroid ? `${currentHeight}px` : 0};
+    flex: 1;
+    background-color: #fafafa;
+`;
+
+export const CategoriesContainer = styled.View`
+    height: 73px;
+    margin-top: 34px;
+`;
+
+export const MenuContainer = styled.View`
+    flex: 1;
+`;
+
+export const Footer = styled.View`
+    min-height: 110px;
+    background-color: #fff;
+    padding: 16px 24px;
+`;
+
+export const FooterContainer = styled.View`
+`;
